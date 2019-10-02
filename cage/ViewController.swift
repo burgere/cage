93cad5a25ae8ea29470f15c4f27aad17d0014cc4
Eric <eric.burger@redbox.com>
2019-10-02 10:47:25 -0500

    init

--- cage/ViewController.swift
+++ cage/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let imageView = UIImageView()
+    var animator: UIDynamicAnimator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(named: "niccage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalToConstant: 100.0),
+            imageView.widthAnchor.constraint(equalToConstant: 100.0)
+        ])
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let gravity = UIGravityBehavior(items: [imageView])
+        animator = UIDynamicAnimator(referenceView: view)
+        animator?.addBehavior(gravity)
+        
+        let collisions = UICollisionBehavior(items: [imageView])
+        collisions.translatesReferenceBoundsIntoBoundary = true
+        
+        animator?.addBehavior(collisions)
+    }
 
 }
 
